68d7507bbf3e82d02f1c0099fd834e664cc0b95a
kim han woong <khw7096@gmail.com>
2019-02-06 09:15:54 +0900

    first

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"syscall/js"
+)
+
+func add(i []js.Value) {
+	js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
+	println(js.ValueOf(i[0].Int() + i[1].Int()).String())
+}
+
+func subtract(i []js.Value) {
+	js.Global().Set("output", js.ValueOf(i[0].Int()-i[1].Int()))
+	println(js.ValueOf(i[0].Int() - i[1].Int()).String())
+}
+
+func registerCallbacks() {
+	js.Global().Set("add", js.NewCallback(add))
+	js.Global().Set("subtract", js.NewCallback(subtract))
+}
+
+func main() {
+	c := make(chan struct{}, 0)
+
+	println("WASM Go Initialized")
+	// register functions
+	registerCallbacks()
+	<-c
+}

94b49da105eefba88a0288d9adb1756dcd69554c
kim han woong <khw7096@gmail.com>
2019-02-06 17:57:47 +0900

    좀더 디버그 할 수 있도록 수정함.

--- app.js
+++ app.js
@@ -1,42 +1,67 @@
+var items = [];
+
 function init() {
     document.getElementById("title").value = "";
     document.getElementById("type").value = "service";
-    document.getElementById("hour").value = "1";
-    document.getElementById("rate").value = "1.00";
-    document.getElementById("charge").value = "55457";
+    document.getElementById("hour").value = 1;
+    document.getElementById("rate").value = 1.0;
+    document.getElementById("charge").value = 55457;
 }
 
 function newItem() {
-    var item = document.getElementById("title").value;
-	if ( item == "" ) {
+    var title = document.getElementById("title").value;
+	if ( title == "" ) {
 		return
 	};
 	var d = new Date();
 	var id = d.getTime();
 	var type = document.getElementById("type").value;
-	var hour = document.getElementById("hour").value;
-	var rate = document.getElementById("rate").value;
-	var charge = document.getElementById("charge").value;
-	var currentTotal = Math.round(Number(hour) * Number(rate) * Number(charge))
+	var hour = Number(document.getElementById("hour").value);
+	var rate = Number(document.getElementById("rate").value);
+	var charge = Number(document.getElementById("charge").value);
+	var subtotal = Math.round(hour * rate * charge)
+	var discount = 1.0;
+	var total = discount * subtotal;
     var ul = document.getElementById("list");
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(`${id} ${type} : ${item} : ${hour}x${rate}x${charge}=${currentTotal}`));
-    ul.appendChild(li);
+	var item = {};
+	item["id"] = id;
+	item["type"] = type;
+	item["title"] = title;
+	item["hour"] = hour;
+	item["rate"] = rate;
+	item["charge"] = charge;
+	item["subtotal"] = subtotal;
+	item["discount"] = discount;
+	item["total"] = total;
+	items.push(item)
+	li.setAttribute("id", id);
+    li.appendChild(document.createTextNode(`${type} : ${title} : ${hour} ${rate} ${charge} ${subtotal} ${discount} ${total}`));
     li.onclick = removeItem;
+    ul.appendChild(li);
 	init()
-	// update total
-	var total = document.getElementById("total").innerHTML;
-	document.getElementById("total").innerHTML = Number(total) + currentTotal
+	updateTotal()
 }
 
-
 function removeItem(e) {
+	id = e.target.getAttribute("id");
+	// remove items
+	for ( i = 0; i < items.length; i++){ 
+	   	if ( items[i]["id"] == id) {
+			 items.splice(i, 1); 
+	   	};
+	};
+	updateTotal();
+	// remove self
     e.target.parentElement.removeChild(e.target);
-	var total = Number(document.getElementById("total").innerHTML);
-	subValue = Number(e.target.innerHTML.split("=")[1])
-	//console.log(subValue)
-	document.getElementById("total").innerHTML = total - subValue
+}
 
+function updateTotal() {
+	var total = 0;
+	for (i = 0; i < items.length; i++) {
+		total += items[i]["subtotal"];
+	}
+	document.getElementById("total").innerHTML = total
 }
 
 function printPage() {

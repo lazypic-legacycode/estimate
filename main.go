403a3e1d1c15e5739a2ba1c5fcde57baeecc5c78
kim hanwoong <khw7096@gmail.com>
2019-02-10 01:09:44 +0900

    add this

--- main.go
+++ main.go
@@ -4,19 +4,21 @@ import (
 	"syscall/js"
 )
 
-func add(i []js.Value) {
+func add(this js.Value, i []js.Value) js.Value {
 	js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
 	println(js.ValueOf(i[0].Int() + i[1].Int()).String())
+	return nil
 }
 
-func subtract(i []js.Value) {
+func subtract(this js.Value i []js.Value) js.Value {
 	js.Global().Set("output", js.ValueOf(i[0].Int()-i[1].Int()))
 	println(js.ValueOf(i[0].Int() - i[1].Int()).String())
+	return nil
 }
 
 func registerCallbacks() {
-	js.Global().Set("add", js.NewCallback(add))
-	js.Global().Set("subtract", js.NewCallback(subtract))
+	js.Global().Set("add", js.FuncOf(add))
+	js.Global().Set("subtract", js.FuncOf(subtract))
 }
 
 func main() {
